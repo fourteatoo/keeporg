b601ff9d0fbf2703203a03adbb9632d183f112a1
Walter Pelissero <walter@pelissero.de>
2026-05-21 23:56:38 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.fourteatoo/keeporg "0.1.0"
+(defproject io.github.fourteatoo/keeporg "0.1.1-SNAPSHOT"
   :description "Google Keep to Emacs Org migration tool"
   :url "http://github.com/fourteatoo/keeporg"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
